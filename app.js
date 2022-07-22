8a251cab34a0f50560a585b94d180b1159a3a9b9
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-07-22 21:54:37 +0300

    style: добавить логи запуска приложения

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
+mongoose
+  .connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => {
     console.log('Database connected.');
   })
@@ -17,4 +18,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 
 app.use(routes);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log('App started.');
+});
