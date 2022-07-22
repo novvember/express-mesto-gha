d5c66f953623279d4ea54599bf4f9ec056a97d22
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-07-22 19:44:07 +0300

    feat: создать модели

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
 
 app.get('/', () => {
   console.log('get request recieved!');
