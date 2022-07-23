5cfe5ffef4fb2d43565a744e47ca788aadc14e05
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-07-23 14:54:37 +0300

    feat: добавить поддержку rest client

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose');
 const { routes } = require('./routes');
 
 const { PORT = 3000 } = process.env;
+const DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 const app = express();
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => {
-    console.log('Database connected.');
+    console.log(`Connected to database on ${DATABASE_URL}`);
   })
   .catch((err) => {
     console.log('Error on database connection');
@@ -28,5 +29,5 @@ app.use((req, res, next) => {
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log('App started.');
+  console.log(`App started on port ${PORT}`);
 });
