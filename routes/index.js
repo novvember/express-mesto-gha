a818d22d6189a87f79b95b1bd7a904d5a8d2a7f8
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-03 21:19:30 +0300

    fix: изменить валидацию avatar

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ routes.post(
       password: Joi.string().required(),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string(),
+      avatar: Joi.string().dataUri(),
     }),
   }),
   createUser,
