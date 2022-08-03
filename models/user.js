bec84b9caf3073dbad93587e947adb2e161f9d4d
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-03 14:02:24 +0300

    feat: настроить значения по умолчанию для полей name, about, avatar при создании пользователя

--- models/user.js
+++ models/user.js
@@ -19,17 +19,17 @@ const userSchema = new mongoose.Schema(
       type: String,
       minlength: 2,
       maxlength: 30,
-      required: true,
+      default: 'Жак-Ив Кусто',
     },
     about: {
       type: String,
       minlength: 2,
       maxlength: 30,
-      required: true,
+      default: 'Исследователь',
     },
     avatar: {
       type: String,
-      required: true,
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
   },
   {
