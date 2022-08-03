f01b65a1a7ecc14f822534072389520036a26559
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-03 13:20:57 +0300

    feat: добавить поля email и password в схему пользователя

--- models/user.js
+++ models/user.js
@@ -2,6 +2,15 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     name: {
       type: String,
       minlength: 2,
