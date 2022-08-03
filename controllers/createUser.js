ef1f082f49c6a50c666b07fa86c0c7b858b4c8ea
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-03 13:56:49 +0300

    feat: добавить хэширование пароля при создании пользователя

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,13 +1,19 @@
+const bcrypt = require('bcryptjs');
 const { User } = require('../models/user');
 const { handleError } = require('../utils/handleError');
 
+const SALT_LENGTH = 10;
+
 async function createUser(req, res) {
   try {
-    const {
-      email, password, name, about, avatar,
-    } = req.body;
+    const { email, password, name, about, avatar } = req.body;
+    const passwordHash = await bcrypt.hash(password, SALT_LENGTH);
     const user = await User.create({
-      email, password, name, about, avatar,
+      email,
+      password: passwordHash,
+      name,
+      about,
+      avatar,
     });
     res.send(user);
   } catch (err) {
