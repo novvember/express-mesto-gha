9f22619318c6643b01ab28ddf2101acf16b9f42c
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-04 13:30:54 +0300

    refactor: вызывать создание документа в бд сразу без проверки существования пользователя

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const { User } = require('../models/user');
-const {ConflictError} = require('../errors/ConflictError');
+const { ConflictError } = require('../errors/ConflictError');
 
 const SALT_LENGTH = 10;
 
@@ -9,22 +9,22 @@ async function createUser(req, res, next) {
     const { email, password, name, about, avatar } = req.body;
     const passwordHash = await bcrypt.hash(password, SALT_LENGTH);
 
-    let user = await User.findOne({email});
+    let user;
 
-    if (user) {
+    try {
+      user = await User.create({
+        email,
+        password: passwordHash,
+        name,
+        about,
+        avatar,
+      });
+    } catch (err) {
       throw new ConflictError('Пользователь с таким email уже существует');
     }
 
-    user = await User.create({
-      email,
-      password: passwordHash,
-      name,
-      about,
-      avatar,
-    });
-
     user = user.toObject();
-    delete user.password
+    delete user.password;
     res.send(user);
   } catch (err) {
     next(err);
