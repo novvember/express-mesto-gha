f36910a55ae266c87d788d7e711442038e4f2e56
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-04 11:59:35 +0300

    feat: добавить валидацию данных для роутов users

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Joi } = require('celebrate');
 
 const { getAllUsers } = require('../controllers/getAllUsers');
 const { getUser } = require('../controllers/getUser');
@@ -10,8 +11,36 @@ const users = express.Router();
 
 users.get('/', getAllUsers);
 users.get('/me', getCurrentUser);
-users.get('/:userId', getUser);
-users.patch('/me', updateUser);
-users.patch('/me/avatar', updateAvatar);
+
+users.get(
+  '/:userId',
+  celebrate({
+    params: Joi.object().keys({
+      userId: Joi.string().alphanum().length(24),
+    }),
+  }),
+  getUser,
+);
+
+users.patch(
+  '/me',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+    }),
+  }),
+  updateUser,
+);
+
+users.patch(
+  '/me/avatar',
+  celebrate({
+    body: Joi.object().keys({
+      avatar: Joi.string().uri(),
+    }),
+  }),
+  updateAvatar,
+);
 
 module.exports = { users };
