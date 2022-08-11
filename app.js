ecefa0e67c79f683bb09bb6890871dfeb21cc7e3
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-08-11 11:16:48 +0300

    feat: подключить express-rate-limit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const { routes } = require('./routes');
 const { handleError } = require('./middlewares/handleError');
@@ -10,6 +11,11 @@ const { PORT = 3000 } = process.env;
 const DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 минут
+  max: 100, // 100 запросов с одного IP
+});
 
 mongoose
   .connect(DATABASE_URL)
@@ -20,6 +26,8 @@ mongoose
     console.log('Error on database connection');
     console.error(err);
   });
+
+app.use(limiter);
 
 app.use(helmet());
 
